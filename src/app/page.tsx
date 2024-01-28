54386ed4115168b64bdd016b84ed18c21e67009d
coldnanee <pmihaylov036@gmail.com>
2024-01-28 18:43:19 +0300

    remove import offer into page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-import { Offer } from '@/components/offer'
-
 function Home() {
-	return (
-		<main className='flex min-h-screen flex-col items-center '>
-			<Offer />
-		</main>
-	)
+	return <main className='flex min-h-screen flex-col items-center '></main>
 }
 
 export default Home
