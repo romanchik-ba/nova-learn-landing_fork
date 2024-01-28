34cf891db592bf92d19ce4f0e2212b63daa52cbb
coldnanee <pmihaylov036@gmail.com>
2024-01-28 18:50:03 +0300

    refactoring stack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import { Stack } from '@/components/stack'
+
 function Home() {
-	return <main className='flex min-h-screen flex-col items-center '></main>
+	return (
+		<main className='flex min-h-screen flex-col items-center '>
+			<Stack />
+		</main>
+	)
 }
 
 export default Home
