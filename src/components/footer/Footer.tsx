fb86d7a7f34d5a53cd160ed4612e4cb74fec8e56
Amir <turgunovamir9@gmail.com>
2024-01-31 14:52:46 +0500

    alteration

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,44 +1,21 @@
-import Link from 'next/link'
+import Image from 'next/image'
 
-import { LogoIcon } from '../icons'
+import LogoIcon from '@/assets/logo.svg'
+
+import { Button } from '../ui'
 
 export const Footer = () => {
 	return (
-		<div className='flex justify-between items-center w-[100%] max-w-[933px] flex-wrap max-[631px]:justify-center gap-[20px] pb-[155px]'>
+		<div className='flex justify-between items-center w-[100%] max-w-[1065px] flex-wrap max-[631px]:justify-center gap-[20px] pb-[155px]'>
 			<div className='max-[630.40px]:hidden'>
-				<LogoIcon />
+				<Image src={LogoIcon} width={121} height={40} alt='logo' />
 			</div>
 			<div className='flex gap-[20px] flex-wrap justify-center'>
-				<Link
-					href='#'
-					className='text-[#b8b7bb] text-[18px] font-normal hover:text-[#28FAFA] transition-all ease-in-out active:text-[#1CADAD]'
-				>
-					Courses
-				</Link>
-				<Link
-					href='#'
-					className='text-[#b8b7bb] text-[18px] font-normal hover:text-[#28FAFA] transition-all ease-in-out active:text-[#1CADAD]'
-				>
-					About us
-				</Link>
-				<Link
-					href='#'
-					className='text-[#b8b7bb] text-[18px] font-normal hover:text-[#28FAFA] transition-all ease-in-out active:text-[#1CADAD]'
-				>
-					Our mentors
-				</Link>
-				<Link
-					href='#'
-					className='text-[#b8b7bb] text-[18px] font-normal hover:text-[#28FAFA] transition-all ease-in-out active:text-[#1CADAD]'
-				>
-					Projects
-				</Link>
-				<Link
-					href='#'
-					className='text-[#b8b7bb] text-[18px] font-normal hover:text-[#28FAFA] transition-all ease-in-out active:text-[#1CADAD]'
-				>
-					News
-				</Link>
+				<Button link='#'>Courses</Button>
+				<Button link='#'>About us</Button>
+				<Button link='#'>Our mentors</Button>
+				<Button link='#'>Projects</Button>
+				<Button link='#'>News</Button>
 			</div>
 		</div>
 	)
