6f84cb8db8414d94428052795726f45a84a613f1
Eduard <>
2024-02-02 02:07:30 +0100

    fix gyration error

--- src/components/news/News.tsx
+++ src/components/news/News.tsx
@@ -8,29 +8,35 @@ export const News = () => {
 			</Title>
 			<div className='grid grid-cols-news grid-rows-news gap-2 transition duration-500'>
 				<div className='grid grid-cols-subgrid col-span-3'>
-					<Text className='relative col-start-1 border-animation-top p-[15px]'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Repudiandae nam, eaque eius iusto itaque Lorem ipsum
-						dolor sit amet consectetur adipisicing elit. Repudiandae
-						nam, eaque eius iusto taque
+					<div className='relative col-start-1 p-[15px]'>
+						<Text>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Repudiandae nam, eaque eius iusto itaque Lorem
+							ipsum dolor sit amet consectetur adipisicing elit.
+							Repudiandae nam, eaque eius iusto taque
+						</Text>
 						<div className='border-animation-left'></div>
-						<div className='border-animation-bottom'></div>
-					</Text>
+						<div className='border-animation-top'></div>
+					</div>
 				</div>
 				<div className='grid grid-cols-subgrid col-span-3'>
-					<Text className='relative col-start-2 p-[15px]'>
-						Lorem ipsum dolor sit amet consectetur adipisicing elit.
-						Repudiandae nam, eaque eius iusto itaque Lorem ipsum
-						dolor sit amet consectetur adipisicing elit. Repudiandae
-						nam, eaque eius iusto itaque
+					<div className='relative col-start-2 p-[15px]'>
+						<Text>
+							Lorem ipsum dolor sit amet consectetur adipisicing
+							elit. Repudiandae nam, eaque eius iusto itaque Lorem
+							ipsum dolor sit amet consectetur adipisicing elit.
+							Repudiandae nam, eaque eius iusto itaque
+						</Text>
+						<div className='border-animation-top'></div>
 						<div className='border-animation-left'></div>
-						<div className='border-animation-bottom'></div>
-					</Text>
+					</div>
 				</div>
 				<div className='grid grid-cols-subgrid col-span-3'>
-					<Button variant='ghost' className='col-start-3 p-[15px]'>
-						More news
-					</Button>
+					<div className='relative col-start-3 p-[15px]'>
+						<Button variant='ghost'>More news</Button>
+						<div className='border-animation-top'></div>
+						<div className='border-animation-left'></div>
+					</div>
 				</div>
 			</div>
 		</section>
