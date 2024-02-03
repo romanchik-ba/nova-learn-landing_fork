42c4afb27b0fb98f423320cc8a65099c1f2b0a75
coldnanee <pmihaylov036@gmail.com>
2024-02-03 20:40:22 +0300

    refactoring code

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -7,17 +7,19 @@ import { Button } from '../ui'
 
 export const Footer = () => {
 	return (
-		<Container className='flex justify-between items-center flex-wrap max-[631px]:justify-center gap-[20px] pb-[155px]'>
-			<div className='max-[630.40px]:hidden'>
-				<Image src={LogoIcon} width={121} height={40} alt='logo' />
-			</div>
-			<div className='flex gap-[20px] flex-wrap justify-center'>
-				<Button link='#'>Courses</Button>
-				<Button link='#'>About us</Button>
-				<Button link='#'>Our mentors</Button>
-				<Button link='#'>Projects</Button>
-				<Button link='#'>News</Button>
-			</div>
-		</Container>
+		<footer>
+			<Container className='flex justify-between items-center flex-wrap max-[631px]:justify-center gap-[20px] pb-[155px]'>
+				<div className='max-[630.40px]:hidden'>
+					<Image src={LogoIcon} width={121} height={40} alt='logo' />
+				</div>
+				<div className='flex gap-[20px] flex-wrap justify-center'>
+					<Button link='#'>Courses</Button>
+					<Button link='#'>About us</Button>
+					<Button link='#'>Our mentors</Button>
+					<Button link='#'>Projects</Button>
+					<Button link='#'>News</Button>
+				</div>
+			</Container>
+		</footer>
 	)
 }
--- src/components/team-banner/TeamBanner.tsx
+++ src/components/team-banner/TeamBanner.tsx
@@ -7,7 +7,7 @@ export const TeamBanner = () => {
 		<section className='w-full mb-[200px]'>
 			<Container className='relative flex flex-col items-center bg-banner-team-gradient rounded-[15px] pt-[74px] pb-[31px]'>
 				<Blink
-					className='opacity-70 -top-[50%] -left-[15%] -z-10'
+					className='opacity-[70%] -top-[50%] -left-[15%] -z-10'
 					color='#311F4D'
 					width='630px'
 					height='580px'
