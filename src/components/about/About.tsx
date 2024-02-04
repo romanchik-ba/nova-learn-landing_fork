377da5f592323d8718829031e693d5db4703331a
Eduard <>
2024-02-04 19:56:11 +0100

    complete about

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,4 +1,5 @@
 import { Container } from '../container'
+import { Blink } from '../icons/Blink'
 import { AboutBG } from '../image/AboutBG'
 import { AboutMessage } from '../image/AboutMessage'
 import { AboutSpin } from '../image/AboutSpin'
@@ -9,6 +10,18 @@ export const About = () => {
 	return (
 		<section className='relative w-full min-h-[757px] mb-[450px]'>
 			<AboutBG />
+			<Blink
+				className='right-0 top-[-100px] opacity-50'
+				color='#3c2571'
+				width='900px'
+				height='900px'
+			/>
+			<Blink
+				className='left-[100px] top-0 opacity-50'
+				color='#3c2571'
+				width='900px'
+				height='900px'
+			/>
 			<Container className='flex'>
 				<div className='w-1/2 pt-[270px]'>
 					<div className='relative flex justify-end items-center py-[65px] px-[40px]'>
