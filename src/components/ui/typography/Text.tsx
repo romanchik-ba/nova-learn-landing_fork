f2d84d7a30de285e03cfa5ebdcc33323cc8daa95
Amir <turgunovamir9@gmail.com>
2024-02-05 21:33:05 +0800

    small fix

--- src/components/ui/typography/Text.tsx
+++ src/components/ui/typography/Text.tsx
@@ -10,7 +10,7 @@ interface titleProps {
 
 export const Text = ({ children, className, transparent }: titleProps) => {
 	const textClasses = classNames(
-		'text-[21px] text-mainText leading-[31.5px] font-nunitoSans sm:text-[16px] md:text-[18px] lg:text-[21px]',
+		'text-mainText leading-[31.5px] font-nunitoSans sm:text-[16px] md:text-[18px] lg:text-[21px]',
 		transparent ? 'opacity-[70%]' : '',
 
 		className || ''
