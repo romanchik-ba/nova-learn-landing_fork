a327d7e8d8389398f564717e65702c97afc1d837
Amir <turgunovamir9@gmail.com>
2024-02-06 21:35:10 +0800

    add text and title sizes for small screen

--- src/components/ui/typography/Text.tsx
+++ src/components/ui/typography/Text.tsx
@@ -9,7 +9,7 @@ interface titleProps {
 
 export const Text = ({ children, className }: titleProps) => {
 	const textClasses = classNames(
-		'text-[20px] text-secondaryText leading-[26px] font-nunitoSans',
+		'text-[14px] text-secondaryText leading-[26px] font-nunitoSans sm:text-[16px] md:text-[18px] lg:text-[20px]',
 		className || ''
 	)
 
