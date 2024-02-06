b741b57742a00a1b513b6a7af8d554354a59ce23
coldnanee <pmihaylov036@gmail.com>
2024-02-06 18:30:49 +0300

    fix bugs && add adaptive && add scroll to top

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from 'next/image'
 
 import LogoIcon from '@/assets/logo.svg'
@@ -6,13 +8,17 @@ import { Container } from '../container'
 import { Button } from '../ui'
 
 export const Footer = () => {
+	const scrollToTop = () => {
+		scrollTo({ top: 0, behavior: 'smooth' })
+	}
+
 	return (
 		<footer>
-			<Container className='flex justify-between items-center flex-wrap max-[631px]:justify-center gap-[20px] pb-[155px]'>
-				<div className='max-[630.40px]:hidden'>
+			<Container className='flex items-center justify-center min-[700px]:justify-between gap-x-[20px] p-[30px]'>
+				<div onClick={scrollToTop}>
 					<Image src={LogoIcon} width={121} height={40} alt='logo' />
 				</div>
-				<div className='flex gap-[20px] flex-wrap justify-center'>
+				<div className='hidden gap-x-[20px] justify-center min-[700px]:flex'>
 					<Button link='#'>Courses</Button>
 					<Button link='#'>About us</Button>
 					<Button link='#'>Our mentors</Button>
