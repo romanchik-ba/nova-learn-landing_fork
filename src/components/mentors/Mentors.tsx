ea9d996ed1c916a67602759ebcbe0949d86ef0bb
coldnanee <pmihaylov036@gmail.com>
2024-02-10 16:00:03 +0300

    fix bugs && add adaptive

--- src/components/mentors/Mentors.tsx
+++ src/components/mentors/Mentors.tsx
@@ -5,8 +5,11 @@ import { MentorsSlider } from './MentorsSlider'
 export const Mentors = () => {
 	return (
 		<section className='w-full relative mb-[250px]'>
-			<Container className='flex justify-between'>
-				<Title variant='h2' className='shrink grow-0 basis-[34%]'>
+			<Container className='flex flex-col justify-between min-[950px]:gap-x-[40px] min-[950px]:flex-row'>
+				<Title
+					variant='h2'
+					className='shrink grow-0 basis-[34%] mb-[30px] min-[950px]:mb-0'
+				>
 					Our mentors
 				</Title>
 				<MentorsSlider />
--- src/components/mentors/MentorsSlider.tsx
+++ src/components/mentors/MentorsSlider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { type MouseEvent, useEffect, useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 import { MentorsSlide } from './MentorsSlide'
 import { mentorItemsArr } from './mentors.data'
@@ -10,8 +10,12 @@ export const MentorsSlider = () => {
 
 	const sliderRef = useRef<HTMLDivElement>(null)
 
-	const onSlideClick = (e: MouseEvent<HTMLDivElement>, index: number) => {
-		if (!sliderRef.current || activeSlide === index) return
+	const isMobile = useRef<boolean>(false)
+
+	const onSlideClick = (index: number) => {
+		if (!sliderRef.current || activeSlide === index || isMobile.current)
+			return
+
 		setActiveSlide(index)
 	}
 
@@ -34,16 +38,18 @@ export const MentorsSlider = () => {
 		}
 	}, [])
 
+	useEffect(() => {
+		isMobile.current = matchMedia('(max-width: 949px)').matches
+	}, [])
+
 	return (
 		<div
 			ref={sliderRef}
-			className='-mr-[200px] overflow-x-hidden scrollbar-disable flex gap-x-[20px] basis-[65%]'
+			className='overflow-x-scroll scrollbar-disable flex gap-x-[20px] min-[950px]:basis-[65%] min-[1445px]:-mr-[188px] '
 		>
 			{mentorItemsArr.map((i, index) => (
 				<MentorsSlide
-					onClick={(e: MouseEvent<HTMLDivElement>) =>
-						onSlideClick(e, index)
-					}
+					onClick={() => onSlideClick(index)}
 					isActiveSlide={index === activeSlide}
 					{...i}
 					key={index}
