26dbb555b3776f12f3ee825490872e6e662986cf
Amir <turgunovamir9@gmail.com>
2024-02-10 11:16:01 +0800

    make adaptive for team banner

--- src/components/team-banner/TeamBanner.tsx
+++ src/components/team-banner/TeamBanner.tsx
@@ -12,16 +12,18 @@ export const TeamBanner = () => {
 					width='630px'
 					height='580px'
 				/>
-				<Text className='uppercase text-[#23DCDC] text-[30px] leading-0 mb-[53px]'>
+				<Text className='uppercase !text-[#23DCDC] text-[24px] leading-0 mb-[40px] sm:text-[30px] md:text-[30px] lg:text-[30px] sm:mb-[53px] md:mb-[53px] lg:mb-[53px]'>
 					Are you ready?
 				</Text>
 				<Title
-					className='max-w-[555px] text-center mb-[61px]'
+					className='max-w-[249px] text-center mb-[40px] text-[42px] sm:text-[64px] md:text-[64px] lg:text-[64px] sm:max-w-[544px] md:max-w-[544px] lg:max-w-[544px] sm:mb-[61px] md:mb-[61px] lg:mb-[61px]'
 					variant='h2'
 				>
 					Be A Part Of Next Team?
 				</Title>
-				<Button className='!px-[34px]'>GET STARTED</Button>
+				<Button className='!px-[34px] mb-[23px] sm:mb-[0px] md:mb-[0px] lg:mb-[0px]'>
+					GET STARTED
+				</Button>
 			</Container>
 		</section>
 	)
